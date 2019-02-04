49007299d48095ead161048fb0f6d97b627a2546
Ian Homer <ian.homer@gmail.com>
2019-02-04 14:57:41 +0000

    Use dependency in gradle task

--- try-kotlin-gradle/build.gradle.kts
+++ try-kotlin-gradle/build.gradle.kts
@@ -1,5 +1,18 @@
+import com.google.common.base.Strings
+
+buildscript {
+    dependencies {
+        classpath("com.google.guava:guava:27.0.1-jre")
+    }
+}
+
+plugins {
+    `java-library`
+}
+
 tasks.register("hello") {
     doLast {
         println("hello")
+        println(Strings.padEnd("A", 10, '.'))
     }
 }
